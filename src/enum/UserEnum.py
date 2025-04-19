f5145a41ca24706df181d51551a88b5c1d4eb407
admin <denis.lamochenko@gmail.com>
2025-04-19 21:57:15 +0800

    usermodel and subjectmodel

---
+++ src/enum/UserEnum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class UserRole(Enum):
+    ADMIN = "ADMIN"
+    STUDENT = "STUDENT"
+    TEACHER = "TEACHER"
+    PARENT = "PARENT"
+    
--- src/models/SubjectModel.py
+++ src/models/SubjectModel.py
@@ -2,28 +2,18 @@ import datetime
 
 from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped, mapped_column, Mapped, relationship
-from src.enum.UserEnum import UserRole
 from sqlalchemy.sql import func
 
 from typing import TYPE_CHECKING
 
 from src.db import Base
 
-class User(Base):
-    __tablename__ = "user_table"
+class Subject(Base):
+    __tablename__ = "subject_table"
 
     id:Mapped[int] = mapped_column(primary_key=True)
 
-    name:Mapped[str]
-    surname:Mapped[str]
-    patronymic:Mapped[str]
-    snils:Mapped[int] = mapped_column(unique=True)
+    name_subject:Mapped[str] = mapped_column(unique=True)
 
-    login:Mapped[str] = mapped_column(unique=True)
-    password:Mapped[bytes]
-    email:Mapped[str] = mapped_column(unique=True)
-    user_role:Mapped[UserRole]
-
-    dob:Mapped[datetime.date]
     createdAt: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updatedAt: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
